77a4757ff174081a6f69655d09d241d1af40cf32
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-06-11 14:01:09 +0200

    Update app.js

--- herregaarde-database/app.js
+++ herregaarde-database/app.js
@@ -2,7 +2,7 @@ console.log("app.js loaded");
 const supabaseUrl = "https://tdxlesfxckkwyaqlaxgj.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkeGxlc2Z4Y2trd3lhcWxheGdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwODY4MjksImV4cCI6MjA5NjY2MjgyOX0.JXltvUgUoyFlh8P7N0ZIznrkkm90gi7mDdE3P02Gak4";
 
-const supabase = window.supabase.createClient(
+const db = window.supabase.createClient(
     supabaseUrl,
     supabaseKey
 );
@@ -13,7 +13,7 @@ document
 
 async function hentData() {
 
-    const { data, error } = await supabase
+    const { data, error } = await db
         .from("Tjenestefolk")
         .select("*")
         .limit(10);
