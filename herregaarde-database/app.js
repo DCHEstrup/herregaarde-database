9f57c69bf04c008097d426841e1c345386e0fcc4
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-06-11 11:59:11 +0200

    Update app.js

--- herregaarde-database/app.js
+++ herregaarde-database/app.js
@@ -1,4 +1,4 @@
-const supabaseUrl = "https://tdxlesfxckkwyaqlaxgj.supabase.co/rest/v1/";
+const supabaseUrl = "https://tdxlesfxckkwyaqlaxgj.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkeGxlc2Z4Y2trd3lhcWxheGdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwODY4MjksImV4cCI6MjA5NjY2MjgyOX0.JXltvUgUoyFlh8P7N0ZIznrkkm90gi7mDdE3P02Gak4";
 
 const supabase = window.supabase.createClient(
