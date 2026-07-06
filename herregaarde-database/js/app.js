ccd5ebb877feb202662852b61cb9c09a4101b187
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-07-06 11:25:56 +0200

    Import and call loadFilters on DOMContentLoaded
    Added loadFilters function call on DOMContentLoaded event.

--- herregaarde-database/js/app.js
+++ herregaarde-database/js/app.js
@@ -1,9 +1,15 @@
 import { getPersonCount } from "./supabase.js";
-
+import { loadFilters } from "./filters.js";
 document.addEventListener("DOMContentLoaded", async () => {
 
     const { count } = await getPersonCount();
 
     console.log(count);
+
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    await loadFilters();
 
 });
