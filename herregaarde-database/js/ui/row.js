63c3d09da141b00abae3711c58fc8844abf298df
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-07-07 14:50:58 +0200

    Update work property to work title in row.js

--- herregaarde-database/js/ui/row.js
+++ herregaarde-database/js/ui/row.js
@@ -13,7 +13,7 @@ export function createRow(person) {
         ·
         ${person.koen ?? "-"}
         <br>
-        ${person.arbejde  ?? ""}
+        ${person.arbejde_titel  ?? ""}
     `;
 row.addEventListener("click", () => {
     // Fjern tidligere markering
