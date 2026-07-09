1c7b71bc894dc2ebbcba353ecd91f4e4e4b1e720
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-07-09 08:37:59 +0200

    Add multi-select functionality for herregårde and years

--- herregaarde-database/js/app.js
+++ herregaarde-database/js/app.js
@@ -7,7 +7,37 @@ import { getCurrentFilters } from "./filtersState.js";
 import { renderPagination } from "./ui/pagination.js";
 import { enableAutosuggest } from "./autosuggest.js";
 import { getJobSuggestions } from "./supabase.js";
+import { createMultiSelect } from "./ui/multiselect.js";
 
+await loadFilters();
+createMultiSelect({
+    containerId: "herregaard",
+    placeholder: "Alle herregårde",
+    values: [
+        "Gammel Estrup",
+        "Frijsenborg",
+        "Rosenholm",
+        "Clausholm",
+        "Løvenholm",
+        "Sostrup",
+        "Krenkerup",
+        "Skjern"
+    ]
+});
+
+createMultiSelect({
+    containerId: "aar",
+    placeholder: "Alle år",
+    values: [
+        1787,
+        1801,
+        1834,
+        1840,
+        1845,
+        1850,
+        1860
+    ]
+});
 /*
 document.addEventListener("DOMContentLoaded", async () => {
     const { data } = await getStatistics();
