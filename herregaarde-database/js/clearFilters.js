fa36c318d73659794b14c332c677c8784de25d2c
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-07-13 08:37:33 +0200

    Add clearFilters function to reset filters
    Implement clearFilters function to reset filter values.

---
+++ herregaarde-database/js/clearFilters.js
@@ -0,0 +1,41 @@
+import { clearMultiSelect } from "./ui/multiselectV2.js";
+
+export function clearFilters() {
+
+    //----------------------------------
+    // Multiselects
+    //----------------------------------
+
+    clearMultiSelect("herregaard");
+    clearMultiSelect("aar");
+
+    //----------------------------------
+    // Arbejde / Position
+    //----------------------------------
+
+    const arbejde =
+        document.getElementById("arbejde");
+    if (arbejde) {
+        arbejde.value = "";
+    }
+
+    //----------------------------------
+    // Fremtidige filtre
+    //----------------------------------
+
+    [
+        "koen",
+        "region",
+        "kommune",
+        "civilstand"
+    ].forEach(id => {
+
+        const element =
+            document.getElementById(id);
+        if (element) {
+            element.value = "";
+        }
+
+    });
+
+}
