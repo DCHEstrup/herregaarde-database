35ac18a657bf7f8001e1f03d37f214bcac78a11f
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-07-13 14:07:41 +0200

    Remove unnecessary console log in search function
    Removed console log for filters in search.js

--- herregaarde-database/js/search.js
+++ herregaarde-database/js/search.js
@@ -8,8 +8,6 @@ export async function performSearch(page = 1) {
         pageSize: 8
     };
 
-
-    console.log(filters);
     const { data, error } = await searchPeople(filters);
     if (error) {
         console.error(error);
