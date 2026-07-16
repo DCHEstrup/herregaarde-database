14ad65de0262c02acffdc15e0df16d26abe03f71
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-07-16 10:02:52 +0200

    Add clearMultiSelect for 'handicaps' filter

--- herregaarde-database/js/clearFilters.js
+++ herregaarde-database/js/clearFilters.js
@@ -14,6 +14,7 @@ export function clearFilters() {
     clearMultiSelect("civilstand");
     clearMultiSelect("region");
     clearMultiSelect("kommune");
+    clearMultiSelect("handicaps");
 
     //----------------------------------
     // Arbejde / Position
