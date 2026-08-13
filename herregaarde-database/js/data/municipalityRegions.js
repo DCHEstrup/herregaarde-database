9cf83b849970cd15cda0180155f36a9c23c091d1
Dansk Center for Herregårdsforskning <dch@gammelestrup.dk>
2026-08-13 13:29:03 +0200

    Create municipalityRegions.js with region data
    Added a new file defining municipality regions in Denmark with corresponding regions.

---
+++ herregaarde-database/js/data/municipalityRegions.js
@@ -0,0 +1,157 @@
+export const municipalityRegions = {
+    //----------------------------------
+    // Hovedstaden
+    //----------------------------------
+
+    "Egedal": "Hovedstaden",
+    "Furesø": "Hovedstaden",
+    "Ishøj": "Hovedstaden",
+    "Lyngby-Taarbæk": "Hovedstaden",
+    "Rudersdal": "Hovedstaden",
+
+    //----------------------------------
+    // Midtjylland
+    //----------------------------------
+
+    "Aarhus": "Midtjylland",
+    "Favrskov": "Midtjylland",
+    "Hedensted": "Midtjylland",
+    "Herning": "Midtjylland",
+    "Holstebro": "Midtjylland",
+    "Horsens": "Midtjylland",
+    "Ikast-Brande": "Midtjylland",
+    "Lemvig": "Midtjylland",
+    "Norddjurs": "Midtjylland",
+    "Odder": "Midtjylland",
+    "Randers": "Midtjylland",
+    "Ringkøbing-Skjern": "Midtjylland",
+    "Samsø": "Midtjylland",
+    "Silkeborg": "Midtjylland",
+    "Skanderborg": "Midtjylland",
+    "Skive": "Midtjylland",
+    "Struer": "Midtjylland",
+    "Syddjurs": "Midtjylland",
+    "Viborg": "Midtjylland",
+
+    //----------------------------------
+    // Nordjylland
+    //----------------------------------
+
+    "Aalborg": "Nordjylland",
+    "Brønderslev": "Nordjylland",
+    "Frederikshavn": "Nordjylland",
+    "Hjørring": "Nordjylland",
+    "Jammerbugt": "Nordjylland",
+    "Mariagerfjord": "Nordjylland",
+    "Morsø": "Nordjylland",
+    "Rebild": "Nordjylland",
+    "Thisted": "Nordjylland",
+    "Vesthimmerlands": "Nordjylland",
+
+    //----------------------------------
+    // Sjælland
+    //----------------------------------
+
+    "Faxe": "Sjælland",
+    "Fredensborg": "Sjælland",
+    "Frederikssund": "Sjælland",
+    "Greve": "Sjælland",
+    "Halsnæs": "Sjælland",
+    "Høje-Taastrup": "Sjælland",
+    "Holbæk": "Sjælland",
+    "Kalundborg": "Sjælland",
+    "Køge": "Sjælland",
+    "Lejre": "Sjælland",
+    "Næstved": "Sjælland",
+    "Odsherred": "Sjælland",
+    "Ringsted": "Sjælland",
+    "Roskilde": "Sjælland",
+    "Slagelse": "Sjælland",
+    "Sorø": "Sjælland",
+    "Stevns": "Sjælland",
+    "Vordingborg": "Sjælland",
+
+    //----------------------------------
+    // Lolland/Falster/Møn
+    //----------------------------------
+
+    "Guldborgsund":
+        "Sjælland; Lolland/Falster/Møn",
+
+    "Lolland":
+        "Sjælland; Lolland/Falster/Møn",
+
+    //----------------------------------
+    // Fyn og øer
+    //----------------------------------
+
+    "Ærø":
+        "Syddanmark; Fyn og øer",
+
+    "Assens":
+        "Syddanmark; Fyn og øer",
+
+    "Bogense":
+        "Syddanmark; Fyn og øer",
+
+    "Faaborg-Midtfyn":
+        "Syddanmark; Fyn og øer",
+
+    "Kerteminde":
+        "Syddanmark; Fyn og øer",
+
+    "Langeland":
+        "Syddanmark; Fyn og øer",
+
+    "Middelfart":
+        "Syddanmark; Fyn og øer",
+
+    "Nordfyns":
+        "Syddanmark; Fyn og øer",
+
+    "Nyborg":
+        "Syddanmark; Fyn og øer",
+
+    "Odense":
+        "Syddanmark; Fyn og øer",
+
+    "Svendborg":
+        "Syddanmark; Fyn og øer",
+
+    //----------------------------------
+    // Syd- og Sønderjylland
+    //----------------------------------
+
+    "Aabenraa":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Billund":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Esbjerg":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Fredericia":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Haderslev":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Kolding":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Sønderborg":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Tønder":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Varde":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Vejen":
+        "Syddanmark; Syd- og Sønderjylland",
+
+    "Vejle":
+        "Syddanmark; Syd- og Sønderjylland"
+};
